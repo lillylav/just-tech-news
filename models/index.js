994bd2c51075532a3a005998a7dd0963702942d8
lillyleiran <lilly.leiran@gmail.com>
2022-03-26 00:02:07 -0600

    completed post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+// many posts belong to 1 user
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// only 1 user belongs to each post
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
 module.exports = { User, Post };
